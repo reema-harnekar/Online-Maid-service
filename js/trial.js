7e7f58a79886fe901f4cc3df218397ba45583b62
Ilaf-kaladiya <kaladiyailaf@gmail.com>
2018-10-25 23:19:41 +0530

    home part one almost done

--- js/trial.js
+++ js/trial.js
@@ -5,3 +5,34 @@ $(document).ready(function(){
         $("#modal1").modal();
     });
 });
+
+
+$(document).ready(function(){
+    $("#btnemployee1").click(function(){
+        $("#modal2").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btnjobseeker1").click(function(){
+        $("#modal3").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btnSignUp").click(function(){
+        $("#modal4").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btnemployee2").click(function(){
+        $("#modal5").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btnjobseeker2").click(function(){
+        $("#modal6").modal();
+    });
+});
